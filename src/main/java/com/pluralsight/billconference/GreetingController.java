392246f14df14b71e7603e3e82bd5cd5493c6277
Bryan <bryankhansen@gmail.com>
2022-12-16 19:50:26 -0700

    Works with JSP

---
+++ src/main/java/com/pluralsight/billconference/GreetingController.java
@@ -0,0 +1,18 @@
+package com.pluralsight.billconference;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @GetMapping
+    public String viewGreeting (Model model) {
+        model.addAttribute("name", "Bill");
+        return "greeting";
+    }
+}
